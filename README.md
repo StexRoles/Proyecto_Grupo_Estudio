6e578316cdb3825c5a12b1680f21625587608def
StexRoles <navarro456123@gmail.com>
2024-04-26 11:16:08 -0500

    creando servidor

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Proyecto_Grupo_Estudio
+# Servicio de tareas
 Sebastian Quintero Jaimes - tienda de ropa 
 
 Juan Camilo Mosquera - plataforma de mercado en línea que ofrece tanto productos físicos (como ropa y artículos usados) como servicios (como tutorías y asistencia con tareas y proyectos). Este mercado integral permite a los usuarios comprar y vender una amplia gama de artículos y servicios, todo en un solo lugar.
---
+++ src/app.js
@@ -0,0 +1,15 @@
+// IMPORTANDO EXPRESS
+const express = require('express');
+const app = express();
+
+// IMPORTANDO PATH
+const path = require('path');
+
+app.get('/inicio', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html')); /* __dirname: Devuelve la ruta del archivo actual */
+});
+
+// CREAR MI SERVIDOR
+app.listen(3000, ()=> {
+    console.log(`Servidor ejecutandose en http://localhost:3000`);
+});
