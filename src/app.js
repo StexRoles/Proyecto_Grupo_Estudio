b41164d270a10407d9a11444ca3ae5a7c3fdc6e6
Camilo Mosquera <camilobladeyt@gmail.com>
2024-05-25 00:47:19 -0500

    html & css for product details mobile version

--- src/app.js
+++ src/app.js
@@ -5,8 +5,14 @@ const app = express();
 // IMPORTANDO PATH
 const path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/inicio', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html')); /* __dirname: Devuelve la ruta del archivo actual */
+});
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'cart_details.html'));
 });
 
 // CREAR MI SERVIDOR
