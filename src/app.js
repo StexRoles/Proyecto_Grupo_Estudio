dcd7e7dc9b304b88f8741fb32d642a55c17279fd
Sebastian Quintero <sebastianjaimesq@gmail.com>
2024-06-07 17:43:33 -0500

    Register agregado

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ app.get('/cart', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'cart_details.html'));
 });
 
+app.get('/cart2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','carrito de comprass','index.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public','register','index.html'));
+});
+
+
 // CREAR MI SERVIDOR
 app.listen(3000, () => {
     console.log(`Servidor ejecutandose en http://localhost:3000`);
